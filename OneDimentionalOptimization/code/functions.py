2ae1aa8846877a551023927c67e169ea25702f51
Dmitroved <dmitroved@gmail.com>
2021-03-14 19:09:08 +0300

    add func calls counter

--- OneDimentionalOptimization/code/functions.py
+++ OneDimentionalOptimization/code/functions.py
@@ -1,9 +1,16 @@
 from math import exp, sin
 
 
-def f1(x):
-    return x ** 2 - 2 * x + exp(-x)
+class Functions:
+    f1_counter = 0
+    f2_counter = 0
 
+    @staticmethod
+    def f1(x):
+        Functions.f1_counter += 1
+        return x ** 2 - 2 * x + exp(-x)
 
-def f2(x):
-    return x ** 3 - 3 * sin(x)
+    @staticmethod
+    def f2(x):
+        Functions.f2_counter += 1
+        return x ** 3 - 3 * sin(x)
--- OneDimentionalOptimization/code/main.py
+++ OneDimentionalOptimization/code/main.py
@@ -2,13 +2,12 @@ from OneDimentionalOptimization.code.functions import *
 from OneDimentionalOptimization.code.Algorithms import dichotomy, fibonacciSequence, fibonacci
 from OneDimentionalOptimization.code.graphics import plotFunctions
 
-
 EPSILON = 0.05
 LEFT_BOARDER = 1
 RIGHT_BOARDER = 1.5
 
-
 if __name__ == '__main__':
+    f1 = Functions.f1
     plotFunctions(f1, LEFT_BOARDER, RIGHT_BOARDER)
 
     count = 0
@@ -24,8 +23,9 @@ if __name__ == '__main__':
     n = 1
     while fibonacciSequence(n) < N:
         n = n + 1
-    interval = fibonacci(f1, LEFT_BOARDER, RIGHT_BOARDER, n, 0, float('inf'), float('inf'), float('inf'), count, EPSILON)
+    interval = fibonacci(f1, LEFT_BOARDER, RIGHT_BOARDER, n, 0, float('inf'), float('inf'), float('inf'), count,
+                         EPSILON)
+    print(interval)
+    print('Вызовов f1', Functions.f1_counter)
     ration = (RIGHT_BOARDER - LEFT_BOARDER) / interval
     print('compression ration is ' + str(ration))
-
-
